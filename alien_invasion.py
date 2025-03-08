9dce80533c903481a7f57b3957a598078bb8428b
Justice <justicemalachi8@gmail.com>
2025-03-08 10:15:00 -0600

    fixed buffering, ship can now move left to right. from desk 3/8/25 10:15am

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,30 +19,44 @@ class AlienInvasion:
         self.ship = Ship(self)
 
 
-
-
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            self._check_events
+            self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
 
 
     def _check_events(self):
-        """respond to keypresses and mouse events"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()   
+            """respong to keypresses and mouse events"""    
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_d:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_a:
+                        self.ship.moving_left = True
+                    
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_d:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_a:
+                        self.ship.moving_left = False
+                        
+
 
     def _update_screen(self):
-        """update images on the screen, and flip to the new screen."""
+        """update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
         pygame.display.flip()
-
+            
 
 if __name__ == '__main__':
     #make a game instance, and run the game.
