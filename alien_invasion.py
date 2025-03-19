71a4c096bdac434bbd399a54fb3de53d0e90a38a
Justice Malachi <justicemalachi8@gmail.com>
2025-03-18 21:48:57 -0500

    Added bullet settings, created bullet class, stored bullets from mac 3/18/25

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -13,10 +14,13 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -24,6 +28,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -48,6 +53,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_a:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys,exit()
                     
 
                 
