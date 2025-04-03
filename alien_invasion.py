7ecf2252cd2613320feb70b803275de76dc3f1d3
Justice Malachi <justicemalachi8@gmail.com>
2025-04-02 19:50:57 -0500

    added alien row, refactored _create_fleet from mac 7:51pm 4/2/25

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,9 +93,23 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        #make a alien
+        #create an elien and keep adding aliens till theres no room left
+        #spacing between aliens is one alien width
+        
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            
+            self._create_alien(current_x)
+            current_x += 2 * alien_width
+    def _create_alien(self, x_position):
+            
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        self.aliens.add(new_alien)
+            
 
     
     def _update_screen(self):
